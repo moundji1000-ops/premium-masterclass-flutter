3d6aeca5ccc388a570b1ab05c6badc480edda54d
moundji1000-ops <moundji1000@gmail.com>
2025-12-06 13:54:51 +0100

    Delete premium_masterclass_full directory

--- premium_masterclass_full/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:premium_masterclass_full/app/app.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MasterClassApp());
-}
